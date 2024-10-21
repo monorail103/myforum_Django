6c6f0bd9139c8ac2a8a568a4aa5d1b4fe26f6e7c
m1031 <ro22062b@osaka-pct.ac.jp>
2024-10-21 21:56:04 +0900

    .gitignore: Add local_settings.py to ignore list

--- myforum/settings.py
+++ myforum/settings.py
@@ -20,9 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f@3!9sufv&rtkn!vt*l_9l6^de+y4m5#7)bc&*7obhts!(&89*'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
